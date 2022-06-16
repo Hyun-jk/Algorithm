610961561881fbe1f7fb3511f0a3bd55566782b4
김재현 <jaehyoen3@gmail.com>
2022-06-16 13:23:03 +0900

    실행 알고리즘 기초

--- CodingTest/implementationAlgorithm/DevelopGame.py
+++ CodingTest/implementationAlgorithm/DevelopGame.py
@@ -25,4 +25,30 @@ count = 1
 turn_time = 0
 while True:
     turn_left()
+    print(f'direction = {direction}')
     nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        print(f'x는{x} y는{y} count는 {count} turn_time 는{turn_time}')
+        continue
+    else:
+        turn_time += 1
+    if turn_time == 4:
+        print(f'네 방향을 다 돌았을 경우')
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        if array[nx][ny] == 0:
+            print('뒤로 한 칸 이동이 가능한 경우')
+            x = nx
+            y = ny
+            print(f'x 는{x} y는 {y}')
+        else:
+            break
+        turn_time  = 0
+print(count)
