2d5cb1eb9513cbe7ebcd19853a31c078169d055d
kim jaehyun <jaehyoen3@gmail.com>
2022-07-05 21:16:03 +0900

    책)정렬 알고리즘

--- book/sortingAlgorithm/182page.py
+++ book/sortingAlgorithm/182page.py
@@ -1,1 +1,16 @@
-#두 배열의 원소 교체
+# 두 배열의 원소 교체
+N, K = map(int, input().split())  # N,K를 입력받기
+A = list(map(int, input().split()))  # 배열 A의 모든 원소를 입력받기
+B = list(map(int, input().split()))  # 배열 B의 모든 원소를 입력 받기
+
+A.sort()  # 배열 A는 오름차순 정렬 수행
+B.sort(reverse=True)  # 배열 B는 내림차순 정렬 수행
+
+for i in range(K):
+    if A[i] < B[i]:  # A의 원소가 B의 원소보다 작은 경우
+        # 두 원소를 교체(스와프)
+        A[i], B[i] = B[i], A[i]
+    else:
+        break
+
+print(sum(A))
