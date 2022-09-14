53825b297db312613178deafccb5b81dd0a3d588
김재현 <jaehyoen3@gmail.com>
2022-09-14 21:29:12 +0900

    Boj)graph 수정

--- website/baekjoonAlgorithm/basic_level/11724.py
+++ website/baekjoonAlgorithm/basic_level/11724.py
@@ -1,3 +1,11 @@
+import sys
+#python 재귀제한이 걸려있어서 재귀 허용치를 넘으면
+#런타임에러를 발생시킨다, 
+sys.setrecursionlimit(10000)
+
+#안해주면 백준에서 시간초과가 걸린다.
+input = sys.stdin.readline 
+
 #연결요소의 개수(graph)
 n,m = map(int,input().split()) #n=노드의 개수, m=간선의 개수
 graph =[[] for _ in range(n+1)]
@@ -9,7 +17,6 @@ for _ in range(m):
 
 def dfs(start):
     visited[start] = True
-    
     for i in graph[start]:
         if visited[i] == False:
             dfs(i)
