a63c23e767843ceba067ef829db3c09ef51dacdf
김재현 <jaehyoen3@gmail.com>
2022-09-14 13:27:12 +0900

    Boj_basic_BFS

---
+++ website/baekjoonAlgorithm/basic_level/1697.py
@@ -0,0 +1,28 @@
+#숨바꼭질(BFS)
+from collections import deque
+"""
+q에 (이동할수 있는 좌표, 이동한 횟수)를 넣고
+bfs(x+1,횟수),bfs(x-1,횟수),bfs(2*x,횟수)를 각각 실행을 시키는 식으로
+코드를 짰는데, 메모리 초과가 나왔다.
+"""
+n ,k = map(int,input().split())
+destination = []
+
+def bfs(n,k):
+    q= deque()
+    q.append(n)
+
+    while q:
+        x = q.popleft()
+        if x == k:
+            print(dist[x])
+            break
+        for nx in (x-1,x+1,x*2):
+             if 0<= nx <= Max and not dist[x]:
+                 dist[nx] = dist[x] +1
+                 q.append(nx)
+   
+Max = 10 ** 5
+dist = [0] *(Max+1)
+
+
