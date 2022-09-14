9e5146f7b384cb13ca9036817f3f77ca186f9fc7
김재현 <jaehyoen3@gmail.com>
2022-09-14 21:02:41 +0900

    boj)DFS 수정

--- website/baekjoonAlgorithm/dfs/11724_DFS.py
+++ website/baekjoonAlgorithm/dfs/11724_DFS.py
@@ -1,27 +1,34 @@
-def dfs(graph,visited,start):
-    visited[start] = True
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
 
-    for i in graph[start]:
-        if not visited[i]:
-            dfs(graph,visited,i)
+n, m = map(int, input().split())
 
-n,m = map(int,input().split())
 graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
 
-for _ in range(m):
-    a,b = map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+#간선의 모든 정보를 graph에 입력
+for _ in range(m): 
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
-visited = [False] * (1+n)
-count =0
 
-for i in range(1, n+1):
-    if not visited[i]:
-        if not graph[i]:
-            count +=1
+def dfs(v):
+    visited[v] = True
+    for i in graph[v]:
+        if visited[i] == False:
             visited[i] = True
-        else:
-            dfs(graph,visited,i)
-            count +=1
-print(count)            
+            dfs(i)
+
+count = 0
+#반복문을 돌려주고, False인 부분을 찾는다.
+#노드의 그룹을 찾기 위해서
+#dfs함수를 돌려주고 false인 부분은 다른 노드와 연결이 되어있지 않는다는 소리임으로, count +=1을 해준다.
+for i in range(1, n+1): 
+    if visited[i] == False:
+        count+=1
+        dfs(i)
+
+print(count)
+
