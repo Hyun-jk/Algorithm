68f4fefeb038a332994ada8097ba0b5a4d599783
김재현 <jaehyoen3@gmail.com>
2022-09-15 21:40:12 +0900

    Boj_basic)Dynamic Programming

---
+++ website/baekjoonAlgorithm/basic_level/16194.py
@@ -0,0 +1,9 @@
+#카드 구매하기2(Dynamic Programming)
+N = int(input())
+p = [0] + list(map(int,input().split()))
+dp = [1e9 for _ in range(N+1)]
+
+for i in range(1,N+1):
+    for k in range(1,i+1):
+        dp[i] = max(dp[i], dp[i-k] + p[k])
+print(dp[i])
